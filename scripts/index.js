80af12258c89ff54b9a866560864c817500e9e89
Stefan <105825004+sstoj1@users.noreply.github.com>
2023-08-25 17:07:37 -0400

    start adding functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 let defaultMode = "Color Mode";
-let defaultGrid = "16";
+let sliderDefault = 16;
+let defaultColor = "red";
+
+window.onload = document.getElementById("color-picker").value = "red";
 
 function updateGridValue(sliderValue) {
   document.getElementById("slider-value").textContent =
@@ -9,5 +12,42 @@ function updateGridValue(sliderValue) {
 }
 
 function setGrid(sliderValue) {
-  // document.getElementById("sketch-container").innerHTML =
+  try {
+    document.getElementById("sketch-container").replaceChildren();
+  } catch (e) {
+    console.log("No Elements To Remove");
+  }
+
+  for (let row = 0; row < sliderValue; row++) {
+    let rowElement = document.createElement("div");
+    rowElement.className = "row";
+
+    for (let column = 0; column < sliderValue; column++) {
+      let cell = document.createElement("div");
+      cell.className = "sketch-cell";
+      cell.setAttribute("onclick", "color()");
+      rowElement.appendChild(cell);
+    }
+    document.getElementById("sketch-container").appendChild(rowElement);
+  }
+}
+
+function resetGrid() {
+  try {
+    document.getElementById("sketch-container").replaceChildren();
+    updateGridValue(sliderDefault);
+    document.getElementById("grid-slider").value = sliderDefault;
+  } catch (e) {
+    console.log("No Elements To Remove");
+  }
+}
+
+function color() {
+  let newBackgroundColor = document
+    .getElementById("color-picker")
+    .getAttribute("value");
+  console.log(newBackgroundColor);
+  document
+    .getElementById("color-picker")
+    .setAttribute("style", "background-color: " + newBackgroundColor);
 }
