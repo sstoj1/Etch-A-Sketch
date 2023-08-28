c8713726a0994c4896597c15605cd4682ba1bd3f
Stefan <105825004+sstoj1@users.noreply.github.com>
2023-08-27 21:56:12 -0400

    add button toggles, comments, and cleanup

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
-let defaultMode = "Color Mode";
-let sliderDefault = 16;
-let defaultColor = "red";
+let sliderDefaultValue = 16;
+let currentColor = "";
+let currentMode = "";
 
-window.onload = document.getElementById("color-picker").value = "red";
-
+/**
+ * Updates text of the <p> on top of the slider to the
+ * dimensions of the grid.
+ *
+ * @param {*} sliderValue
+ */
 function updateGridValue(sliderValue) {
   document.getElementById("slider-value").textContent =
     sliderValue + " x " + sliderValue;
@@ -11,12 +15,13 @@ function updateGridValue(sliderValue) {
   setGrid(sliderValue);
 }
 
+/**
+ * Creates a grid of divs based on the value of the slider.
+ *
+ * @param {*} sliderValue
+ */
 function setGrid(sliderValue) {
-  try {
-    document.getElementById("sketch-container").replaceChildren();
-  } catch (e) {
-    console.log("No Elements To Remove");
-  }
+  document.getElementById("sketch-container").replaceChildren();
 
   for (let row = 0; row < sliderValue; row++) {
     let rowElement = document.createElement("div");
@@ -25,29 +30,41 @@ function setGrid(sliderValue) {
     for (let column = 0; column < sliderValue; column++) {
       let cell = document.createElement("div");
       cell.className = "sketch-cell";
-      cell.setAttribute("onclick", "color()");
+      cell.setAttribute("onmouseover", "color()");
       rowElement.appendChild(cell);
     }
     document.getElementById("sketch-container").appendChild(rowElement);
   }
 }
 
+/**
+ * Resets the sketch grid to 16x16
+ */
 function resetGrid() {
-  try {
-    document.getElementById("sketch-container").replaceChildren();
-    updateGridValue(sliderDefault);
-    document.getElementById("grid-slider").value = sliderDefault;
-  } catch (e) {
-    console.log("No Elements To Remove");
-  }
+  document.getElementById("sketch-container").replaceChildren();
+  updateGridValue(sliderDefaultValue);
+  document.getElementById("grid-slider").value = sliderDefaultValue;
 }
 
-function color() {
-  let newBackgroundColor = document
-    .getElementById("color-picker")
-    .getAttribute("value");
-  console.log(newBackgroundColor);
-  document
-    .getElementById("color-picker")
-    .setAttribute("style", "background-color: " + newBackgroundColor);
+function setCurrentMode(mode) {
+  switch (mode) {
+    case "color":
+      document.getElementById("color-button").style.backgroundColor = "#a86b5b";
+      document.getElementById("rainbow-button").style.backgroundColor =
+        "#7f97a2";
+      document.getElementById("erase-button").style.backgroundColor = "#7f97a2";
+      break;
+    case "rainbow":
+      document.getElementById("rainbow-button").style.backgroundColor =
+        "#a86b5b";
+      document.getElementById("erase-button").style.backgroundColor = "#7f97a2";
+      document.getElementById("color-button").style.backgroundColor = "#7f97a2";
+      break;
+    case "eraser":
+      document.getElementById("erase-button").style.backgroundColor = "#a86b5b";
+      document.getElementById("color-button").style.backgroundColor = "#7f97a2";
+      document.getElementById("rainbow-button").style.backgroundColor =
+        "#7f97a2";
+      break;
+  }
 }
